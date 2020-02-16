f61feee6cfb7ecb76a921f03ed23c8223e17095c
Akshay Nailwal <akshaynailwal999@gmail.com>
2020-02-16 16:15:38 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let { load_and_search } = require("./functions")
 
-load_and_search().then(data=>console.log(data))
+// load_and_search(method="file",path="./data.txt", location=[53.339428, -6.257664], distance=100).then(data => console.log(data))
 
 module.exports = {
     load_and_search,
-    
 }
--- load.js
+++ load.js
@@ -1,8 +1,10 @@
 let fs = require("fs");
+let path = require("path")
 let process = require("./process");
 
 function load_from_file(file_path, cb) {
-    fs.readFile(file_path, "ascii", function (err, data) {
+    f_path = path.join(__dirname,file_path)
+    fs.readFile(f_path, "ascii", function (err, data) {
         cb(err, data);
     });
 }
